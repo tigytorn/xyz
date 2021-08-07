37a30b218123bc6fed6109c83af36b0c3c82eff7
tigytorn <86531503+tigytorn@users.noreply.github.com>
2021-08-07 11:29:58 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+function setup() {
+  createCanvas(400,400);
+   ball=createSprite(200,200,20,20);
+ball.shapeColor="blue";
+}
+
+function draw() 
+{
+  background("red");
+if(keyIsDown(RIGHT_ARROW)){
+  ball.position.x=ball.position.x+2;
+}
+if(keyIsDown(LEFT_ARROW)){
+  ball.position.x=ball.position.x-2;
+}
+if(keyIsDown(UP_ARROW)){
+  ball.position.y=ball.position.y-2;
+}
+if(keyIsDown(DOWN_ARROW)){
+  ball.position.y=ball.position.y+2;
+} 
+drawSprites();
+
+}
+
+
+
+
